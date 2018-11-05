13ec074310468b8ec4ec06ecde73d1ccc66626b9
hustlijian <hustlijian@gmail.com>
2018-11-05 22:21:11 +0800

    update: 更新获取脚本，封装功能，文件保存到分离目录

--- seek.py
+++ seek.py
@@ -1,22 +1,16 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import sys
 import hashlib
 import requests
 import urllib
+import errno
 from HTMLParser import HTMLParser
 from urlparse import urljoin, urldefrag
 
-url = 'https://mp.weixin.qq.com/s/2wnIRBpj0keOnScHVUexZg'
-main_file = 'index.html'
-
 
 def get_links_from_url(url, html):
-    """Download the page at `url` and parse it for links.
-
-    Returned links have had the fragment after `#` removed, and have been made
-    absolute so, e.g. the URL 'gen.html#tornado.gen.coroutine' becomes
-    'http://www.tornadoweb.org/en/stable/gen.html'.
-    """
     urls = dict()
     try:
         orig_urls = get_links(html)
@@ -24,7 +18,6 @@ def get_links_from_url(url, html):
             urls[item] = urljoin(url, remove_fragment(item))
     except Exception as e:
         print('Exception: %s %s' % (e, url))
-
     return urls
 
 
@@ -45,7 +38,6 @@ def get_links(html):
             #     self.urls.append(href)
 
             if tag == 'img':
-                print attrs
                 src = dict(attrs).get('data-src') or dict(attrs).get('src')
                 if src:
                     self.urls.append(src)
@@ -60,24 +52,39 @@ def get_hash_name(input):
     return hash_object.hexdigest()
 
 
-def test(url):
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def get_url(url):
+    basepath = get_hash_name(url)
+    mkdir_p(basepath)
     rsp = requests.get(url)
-    print rsp
     html = rsp.text
     html = html.encode(rsp.encoding)
 
     urls = get_links_from_url(url, html)
     for orig, url in urls.iteritems():
         filetype = "png"
-        name = "%s.%s" % (get_hash_name(orig), filetype)
-        print name
+        name = "%s/%s.%s" % (basepath, get_hash_name(orig), filetype)
         urllib.urlretrieve(url, name)
         html = html.replace(orig, "/" + name)
 
+    main_file = "%s/index.html" % basepath
     with open(main_file, 'w') as fd:
         fd.write(html)
 
+    return main_file
+
 
 if __name__ == '__main__':
-    url = 'https://mp.weixin.qq.com/s/2wnIRBpj0keOnScHVUexZg'
-    test(url)
+    url = 'https://mp.weixin.qq.com/s/uHtEI_spD2slM-ddVQ9reA'
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    print get_url(url)
